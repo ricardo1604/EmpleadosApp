97ad550ee3cb1388871b5186181217a2b0c30a8f
Ricardo Sosa <rsosa.ramos@gmail.com>
2023-02-03 08:17:20 -0600

    Init Commit

---
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-empleados',
+  templateUrl: './list-empleados.component.html',
+  styleUrls: ['./list-empleados.component.css']
+})
+export class ListEmpleadosComponent {
+
+}
